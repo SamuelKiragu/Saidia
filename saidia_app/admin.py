689ddad1d780e9c6cc611b690102f7782c4baa92
SamuelKiragu <samuelkiragu107@gmail.com>
2021-12-10 11:27:30 +0300

    added the admin functionality

---
+++ saidia_app/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+from saidia_app.models import *
+
+# Register your models here.
+admin.site.register(Orphanage)
+admin.site.register(Review)
+admin.site.register(Manager)
+admin.site.register(Location)
+admin.site.register(Needs)
+admin.site.register(User)
